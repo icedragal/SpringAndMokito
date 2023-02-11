ba6c3865f08e21cd20914f87a98dcd4131c367a2
Icedragal <icedragal@gmail.com>
2023-02-11 17:08:03 +0300

    Initial commit

---
+++ src/main/java/me/egorzhuravlev/springandmokito/SpringAndMokitoApplication.java
@@ -0,0 +1,13 @@
+package me.egorzhuravlev.springandmokito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndMokitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndMokitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/egorzhuravlev/springandmokito/SpringAndMokitoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.egorzhuravlev.springandmokito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndMokitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
